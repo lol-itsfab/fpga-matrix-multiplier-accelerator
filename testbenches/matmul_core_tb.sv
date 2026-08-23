222a33088bc9c5307646d04c8e5ef0804fd202e9
lol-itsfab <ffabian.m40@gmail.com>
2026-08-23 09:56:24 -0400

    Verified multi-term accumulation and full 8 x 8 multiplication with all distinct values

--- testbenches/matmul_core_tb.sv
+++ testbenches/matmul_core_tb.sv
@@ -116,7 +116,29 @@ module matmul_core_tb;
         do_read("mat_a_independence", 2'b00, 3'd0, 3'd0, 32'h0000AAAA);
         do_read("mat_b_independence", 2'b01, 3'd0, 3'd0, 32'h0000BBBB);
 
-        // need to re-zero all entries after prev populated entries
+        for (c = 0; c < 8; c = c + 1) begin
+            for (r = 0; r < 8; r = r + 1) begin
+                do_write(1'b0, r[2:0], c[2:0], 16'd0);
+                do_write(1'b1, r[2:0], c[2:0], 16'd0);
+            end
+        end
+
+        do_write(1'b0, 3'd0, 3'd0, 16'd256);
+        do_write(1'b1, 3'd0, 3'd0, 16'd256);
+        do_write(1'b0, 3'd0, 3'd1, 16'd512);
+        do_write(1'b1, 3'd1, 3'd0, 16'd256);
+        do_write(1'b0, 3'd0, 3'd2, 16'd256);
+        do_write(1'b1, 3'd2, 3'd0, 16'd768);
+
+        start = 1'b1;
+        @(posedge clk);
+        #1;
+        start = 1'b0;
+        wait (done == 1'b1);
+
+        do_read("mat_c_multiterm", 2'b10, 3'd0, 3'd0, 32'd393216);
+
+        // full 8 x 8 multiplication with all distinct values.
         for (r = 0; r < 8; r = r + 1) begin
             for (c = 0; c < 8; c = c + 1) begin
                 do_write(1'b0, r[2:0], c[2:0], 16'd0);
@@ -124,27 +146,213 @@ module matmul_core_tb;
             end
         end
 
-        // mat_a[0][0]= 1.0 (Q8.8 = 256), mat_b[0][0]= 2.0 (Q8.8 = 512)
-        do_write(1'b0, 3'd0, 3'd0, 16'd256);
-        do_write(1'b1, 3'd0, 3'd0, 16'd512);
+        // loading mat_a entry values
+         do_write(1'b0, 3'd0, 3'd0, 16'd256);
+        do_write(1'b0, 3'd0, 3'd1, 16'd512);
+        do_write(1'b0, 3'd0, 3'd2, 16'd768);
+        do_write(1'b0, 3'd0, 3'd3, 16'd1024);
+        do_write(1'b0, 3'd0, 3'd4, 16'd1280);
+        do_write(1'b0, 3'd0, 3'd5, 16'd1536);
+        do_write(1'b0, 3'd0, 3'd6, 16'd1792);
+        do_write(1'b0, 3'd0, 3'd7, 16'd2048);
+        do_write(1'b0, 3'd1, 3'd0, 16'd512);
+        do_write(1'b0, 3'd1, 3'd1, 16'd768);
+        do_write(1'b0, 3'd1, 3'd2, 16'd1024);
+        do_write(1'b0, 3'd1, 3'd3, 16'd1280);
+        do_write(1'b0, 3'd1, 3'd4, 16'd1536);
+        do_write(1'b0, 3'd1, 3'd5, 16'd1792);
+        do_write(1'b0, 3'd1, 3'd6, 16'd2048);
+        do_write(1'b0, 3'd1, 3'd7, 16'd256);
+        do_write(1'b0, 3'd2, 3'd0, 16'd768);
+        do_write(1'b0, 3'd2, 3'd1, 16'd1024);
+        do_write(1'b0, 3'd2, 3'd2, 16'd1280);
+        do_write(1'b0, 3'd2, 3'd3, 16'd1536);
+        do_write(1'b0, 3'd2, 3'd4, 16'd1792);
+        do_write(1'b0, 3'd2, 3'd5, 16'd2048);
+        do_write(1'b0, 3'd2, 3'd6, 16'd256);
+        do_write(1'b0, 3'd2, 3'd7, 16'd512);
+        do_write(1'b0, 3'd3, 3'd0, 16'd1024);
+        do_write(1'b0, 3'd3, 3'd1, 16'd1280);
+        do_write(1'b0, 3'd3, 3'd2, 16'd1536);
+        do_write(1'b0, 3'd3, 3'd3, 16'd1792);
+        do_write(1'b0, 3'd3, 3'd4, 16'd2048);
+        do_write(1'b0, 3'd3, 3'd5, 16'd256);
+        do_write(1'b0, 3'd3, 3'd6, 16'd512);
+        do_write(1'b0, 3'd3, 3'd7, 16'd768);
+        do_write(1'b0, 3'd4, 3'd0, 16'd1280);
+        do_write(1'b0, 3'd4, 3'd1, 16'd1536);
+        do_write(1'b0, 3'd4, 3'd2, 16'd1792);
+        do_write(1'b0, 3'd4, 3'd3, 16'd2048);
+        do_write(1'b0, 3'd4, 3'd4, 16'd256);
+        do_write(1'b0, 3'd4, 3'd5, 16'd512);
+        do_write(1'b0, 3'd4, 3'd6, 16'd768);
+        do_write(1'b0, 3'd4, 3'd7, 16'd1024);
+        do_write(1'b0, 3'd5, 3'd0, 16'd1536);
+        do_write(1'b0, 3'd5, 3'd1, 16'd1792);
+        do_write(1'b0, 3'd5, 3'd2, 16'd2048);
+        do_write(1'b0, 3'd5, 3'd3, 16'd256);
+        do_write(1'b0, 3'd5, 3'd4, 16'd512);
+        do_write(1'b0, 3'd5, 3'd5, 16'd768);
+        do_write(1'b0, 3'd5, 3'd6, 16'd1024);
+        do_write(1'b0, 3'd5, 3'd7, 16'd1280);
+        do_write(1'b0, 3'd6, 3'd0, 16'd1792);
+        do_write(1'b0, 3'd6, 3'd1, 16'd2048);
+        do_write(1'b0, 3'd6, 3'd2, 16'd256);
+        do_write(1'b0, 3'd6, 3'd3, 16'd512);
+        do_write(1'b0, 3'd6, 3'd4, 16'd768);
+        do_write(1'b0, 3'd6, 3'd5, 16'd1024);
+        do_write(1'b0, 3'd6, 3'd6, 16'd1280);
+        do_write(1'b0, 3'd6, 3'd7, 16'd1536);
+        do_write(1'b0, 3'd7, 3'd0, 16'd2048);
+        do_write(1'b0, 3'd7, 3'd1, 16'd256);
+        do_write(1'b0, 3'd7, 3'd2, 16'd512);
+        do_write(1'b0, 3'd7, 3'd3, 16'd768);
+        do_write(1'b0, 3'd7, 3'd4, 16'd1024);
+        do_write(1'b0, 3'd7, 3'd5, 16'd1280);
+        do_write(1'b0, 3'd7, 3'd6, 16'd1536);
+        do_write(1'b0, 3'd7, 3'd7, 16'd1792);
 
-        // pulse start
+        // loading mat_b entry values
+        do_write(1'b1, 3'd0, 3'd0, 16'd256);
+        do_write(1'b1, 3'd0, 3'd1, 16'd512);
+        do_write(1'b1, 3'd0, 3'd2, 16'd768);
+        do_write(1'b1, 3'd0, 3'd3, 16'd1024);
+        do_write(1'b1, 3'd0, 3'd4, 16'd1280);
+        do_write(1'b1, 3'd0, 3'd5, 16'd1536);
+        do_write(1'b1, 3'd0, 3'd6, 16'd1792);
+        do_write(1'b1, 3'd0, 3'd7, 16'd2048);
+        do_write(1'b1, 3'd1, 3'd0, 16'd768);
+        do_write(1'b1, 3'd1, 3'd1, 16'd1024);
+        do_write(1'b1, 3'd1, 3'd2, 16'd1280);
+        do_write(1'b1, 3'd1, 3'd3, 16'd1536);
+        do_write(1'b1, 3'd1, 3'd4, 16'd1792);
+        do_write(1'b1, 3'd1, 3'd5, 16'd2048);
+        do_write(1'b1, 3'd1, 3'd6, 16'd256);
+        do_write(1'b1, 3'd1, 3'd7, 16'd512);
+        do_write(1'b1, 3'd2, 3'd0, 16'd1280);
+        do_write(1'b1, 3'd2, 3'd1, 16'd1536);
+        do_write(1'b1, 3'd2, 3'd2, 16'd1792);
+        do_write(1'b1, 3'd2, 3'd3, 16'd2048);
+        do_write(1'b1, 3'd2, 3'd4, 16'd256);
+        do_write(1'b1, 3'd2, 3'd5, 16'd512);
+        do_write(1'b1, 3'd2, 3'd6, 16'd768);
+        do_write(1'b1, 3'd2, 3'd7, 16'd1024);
+        do_write(1'b1, 3'd3, 3'd0, 16'd1792);
+        do_write(1'b1, 3'd3, 3'd1, 16'd2048);
+        do_write(1'b1, 3'd3, 3'd2, 16'd256);
+        do_write(1'b1, 3'd3, 3'd3, 16'd512);
+        do_write(1'b1, 3'd3, 3'd4, 16'd768);
+        do_write(1'b1, 3'd3, 3'd5, 16'd1024);
+        do_write(1'b1, 3'd3, 3'd6, 16'd1280);
+        do_write(1'b1, 3'd3, 3'd7, 16'd1536);
+        do_write(1'b1, 3'd4, 3'd0, 16'd256);
+        do_write(1'b1, 3'd4, 3'd1, 16'd512);
+        do_write(1'b1, 3'd4, 3'd2, 16'd768);
+        do_write(1'b1, 3'd4, 3'd3, 16'd1024);
+        do_write(1'b1, 3'd4, 3'd4, 16'd1280);
+        do_write(1'b1, 3'd4, 3'd5, 16'd1536);
+        do_write(1'b1, 3'd4, 3'd6, 16'd1792);
+        do_write(1'b1, 3'd4, 3'd7, 16'd2048);
+        do_write(1'b1, 3'd5, 3'd0, 16'd768);
+        do_write(1'b1, 3'd5, 3'd1, 16'd1024);
+        do_write(1'b1, 3'd5, 3'd2, 16'd1280);
+        do_write(1'b1, 3'd5, 3'd3, 16'd1536);
+        do_write(1'b1, 3'd5, 3'd4, 16'd1792);
+        do_write(1'b1, 3'd5, 3'd5, 16'd2048);
+        do_write(1'b1, 3'd5, 3'd6, 16'd256);
+        do_write(1'b1, 3'd5, 3'd7, 16'd512);
+        do_write(1'b1, 3'd6, 3'd0, 16'd1280);
+        do_write(1'b1, 3'd6, 3'd1, 16'd1536);
+        do_write(1'b1, 3'd6, 3'd2, 16'd1792);
+        do_write(1'b1, 3'd6, 3'd3, 16'd2048);
+        do_write(1'b1, 3'd6, 3'd4, 16'd256);
+        do_write(1'b1, 3'd6, 3'd5, 16'd512);
+        do_write(1'b1, 3'd6, 3'd6, 16'd768);
+        do_write(1'b1, 3'd6, 3'd7, 16'd1024);
+        do_write(1'b1, 3'd7, 3'd0, 16'd1792);
+        do_write(1'b1, 3'd7, 3'd1, 16'd2048);
+        do_write(1'b1, 3'd7, 3'd2, 16'd256);
+        do_write(1'b1, 3'd7, 3'd3, 16'd512);
+        do_write(1'b1, 3'd7, 3'd4, 16'd768);
+        do_write(1'b1, 3'd7, 3'd5, 16'd1024);
+        do_write(1'b1, 3'd7, 3'd6, 16'd1280);
+        do_write(1'b1, 3'd7, 3'd7, 16'd1536);
+
+        // multiply
         start = 1'b1;
         @(posedge clk);
         #1;
         start = 1'b0;
+        wait (done == 1'b1);
 
-        // wait for done (pauses execution until condition is true (1))
-        wait(done == 1'b1);
-
-        // here we will check the result -> 1.0 * 2.0 = 2.0, in Q16.16 it is 2 * 65536 = 131072
-        do_read("mat_c_0_0", 2'b10, 3'd0, 3'd0, 32'd131072);
-
-        // check that everything else is still 0
-        do_read("mat_c_1_1_zero", 2'b10, 3'd1, 3'd1, 32'd0);
-        do_read("mat_c_7_7_zero", 2'b10, 3'd7, 3'd7, 32'd0);
+        // check all 64 outputs
+        do_read("mat_c_0_0", 2'b10, 3'd0, 3'd0, 32'd10747904);
+        do_read("mat_c_0_1", 2'b10, 3'd0, 3'd1, 32'd13107200);
+        do_read("mat_c_0_2", 2'b10, 3'd0, 3'd2, 32'd9175040);
+        do_read("mat_c_0_3", 2'b10, 3'd0, 3'd3, 32'd11534336);
+        do_read("mat_c_0_4", 2'b10, 3'd0, 3'd4, 32'd8650752);
+        do_read("mat_c_0_5", 2'b10, 3'd0, 3'd5, 32'd11010048);
+        do_read("mat_c_0_6", 2'b10, 3'd0, 3'd6, 32'd9175040);
+        do_read("mat_c_0_7", 2'b10, 3'd0, 3'd7, 32'd11534336);
+        do_read("mat_c_1_0", 2'b10, 3'd1, 3'd0, 32'd9175040);
+        do_read("mat_c_1_1", 2'b10, 3'd1, 3'd1, 32'd11534336);
+        do_read("mat_c_1_2", 2'b10, 3'd1, 3'd2, 32'd10747904);
+        do_read("mat_c_1_3", 2'b10, 3'd1, 3'd3, 32'd13107200);
+        do_read("mat_c_1_4", 2'b10, 3'd1, 3'd4, 32'd9175040);
+        do_read("mat_c_1_5", 2'b10, 3'd1, 3'd5, 32'd11534336);
+        do_read("mat_c_1_6", 2'b10, 3'd1, 3'd6, 32'd8650752);
+        do_read("mat_c_1_7", 2'b10, 3'd1, 3'd7, 32'd11010048);
+        do_read("mat_c_2_0", 2'b10, 3'd2, 3'd0, 32'd8650752);
+        do_read("mat_c_2_1", 2'b10, 3'd2, 3'd1, 32'd11010048);
+        do_read("mat_c_2_2", 2'b10, 3'd2, 3'd2, 32'd9175040);
+        do_read("mat_c_2_3", 2'b10, 3'd2, 3'd3, 32'd11534336);
+        do_read("mat_c_2_4", 2'b10, 3'd2, 3'd4, 32'd10747904);
+        do_read("mat_c_2_5", 2'b10, 3'd2, 3'd5, 32'd13107200);
+        do_read("mat_c_2_6", 2'b10, 3'd2, 3'd6, 32'd9175040);
+        do_read("mat_c_2_7", 2'b10, 3'd2, 3'd7, 32'd11534336);
+        do_read("mat_c_3_0", 2'b10, 3'd3, 3'd0, 32'd9175040);
+        do_read("mat_c_3_1", 2'b10, 3'd3, 3'd1, 32'd11534336);
+        do_read("mat_c_3_2", 2'b10, 3'd3, 3'd2, 32'd8650752);
+        do_read("mat_c_3_3", 2'b10, 3'd3, 3'd3, 32'd11010048);
+        do_read("mat_c_3_4", 2'b10, 3'd3, 3'd4, 32'd9175040);
+        do_read("mat_c_3_5", 2'b10, 3'd3, 3'd5, 32'd11534336);
+        do_read("mat_c_3_6", 2'b10, 3'd3, 3'd6, 32'd10747904);
+        do_read("mat_c_3_7", 2'b10, 3'd3, 3'd7, 32'd13107200);
+        do_read("mat_c_4_0", 2'b10, 3'd4, 3'd0, 32'd10747904);
+        do_read("mat_c_4_1", 2'b10, 3'd4, 3'd1, 32'd13107200);
+        do_read("mat_c_4_2", 2'b10, 3'd4, 3'd2, 32'd9175040);
+        do_read("mat_c_4_3", 2'b10, 3'd4, 3'd3, 32'd11534336);
+        do_read("mat_c_4_4", 2'b10, 3'd4, 3'd4, 32'd8650752);
+        do_read("mat_c_4_5", 2'b10, 3'd4, 3'd5, 32'd11010048);
+        do_read("mat_c_4_6", 2'b10, 3'd4, 3'd6, 32'd9175040);
+        do_read("mat_c_4_7", 2'b10, 3'd4, 3'd7, 32'd11534336);
+        do_read("mat_c_5_0", 2'b10, 3'd5, 3'd0, 32'd9175040);
+        do_read("mat_c_5_1", 2'b10, 3'd5, 3'd1, 32'd11534336);
+        do_read("mat_c_5_2", 2'b10, 3'd5, 3'd2, 32'd10747904);
+        do_read("mat_c_5_3", 2'b10, 3'd5, 3'd3, 32'd13107200);
+        do_read("mat_c_5_4", 2'b10, 3'd5, 3'd4, 32'd9175040);
+        do_read("mat_c_5_5", 2'b10, 3'd5, 3'd5, 32'd11534336);
+        do_read("mat_c_5_6", 2'b10, 3'd5, 3'd6, 32'd8650752);
+        do_read("mat_c_5_7", 2'b10, 3'd5, 3'd7, 32'd11010048);
+        do_read("mat_c_6_0", 2'b10, 3'd6, 3'd0, 32'd8650752);
+        do_read("mat_c_6_1", 2'b10, 3'd6, 3'd1, 32'd11010048);
+        do_read("mat_c_6_2", 2'b10, 3'd6, 3'd2, 32'd9175040);
+        do_read("mat_c_6_3", 2'b10, 3'd6, 3'd3, 32'd11534336);
+        do_read("mat_c_6_4", 2'b10, 3'd6, 3'd4, 32'd10747904);
+        do_read("mat_c_6_5", 2'b10, 3'd6, 3'd5, 32'd13107200);
+        do_read("mat_c_6_6", 2'b10, 3'd6, 3'd6, 32'd9175040);
+        do_read("mat_c_6_7", 2'b10, 3'd6, 3'd7, 32'd11534336);
+        do_read("mat_c_7_0", 2'b10, 3'd7, 3'd0, 32'd9175040);
+        do_read("mat_c_7_1", 2'b10, 3'd7, 3'd1, 32'd11534336);
+        do_read("mat_c_7_2", 2'b10, 3'd7, 3'd2, 32'd8650752);
+        do_read("mat_c_7_3", 2'b10, 3'd7, 3'd3, 32'd11010048);
+        do_read("mat_c_7_4", 2'b10, 3'd7, 3'd4, 32'd9175040);
+        do_read("mat_c_7_5", 2'b10, 3'd7, 3'd5, 32'd11534336);
+        do_read("mat_c_7_6", 2'b10, 3'd7, 3'd6, 32'd10747904);
+        do_read("mat_c_7_7", 2'b10, 3'd7, 3'd7, 32'd13107200);
 
         $display("---- matmul_core Testbench end ----");
+        $display("Passed: %0d   Failed: %0d", pass_count, fail_count);
         if (fail_count == 0)
             $display("ALL TESTS PASSED");
         else
